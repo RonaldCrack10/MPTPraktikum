b8772e0a3fce63c77723ab9d7ec5ad0510d6c2b3
RonaldCrack10 <rashidgaming6@gmail.com>
2026-04-26 13:22:19 +0200

    aufgabe 3

--- webcam/aufgabe3.py
+++ webcam/aufgabe3.py
@@ -1,4 +1,15 @@
 import cv2
 
-def histogramam():
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print('cannot access to the camera')
+    exit()
+while True:
+    ret, frame = cap.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY )
+    cv2.imshow('Camera', gray_frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+def drawHistogram(gray_image):
     pass
